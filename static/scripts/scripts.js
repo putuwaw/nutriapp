8516e2c69d665b6029065ebebc46a3910347d97a
Putu Widyantara <putuwaw973@gmail.com>
2022-12-19 15:34:10 +0800

    finishing

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -4,7 +4,7 @@ var graphviz = d3.select("#graph").graphviz().on("initEnd", render);
 function render() {
     var dot = dots[0].join('');
     // scale graph to fit in the window
-    const width = window.innerWidth / 1.5;
+    const width = window.innerWidth / 1.5 - 22;
     const height = window.innerHeight / 1.5;
     graphviz
         .width(width)
